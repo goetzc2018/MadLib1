c1c1ba1ea4eaf7dcdaaf80b3605113ce8ce80d5d
goetzc2018 <cgoetz@sjnma.org>
2018-05-18 08:32:29 -0500

    did project

---
+++ main.js
@@ -0,0 +1,13 @@
+var noun1 =  prompt("enter a noun", "");
+var noun2 = prompt ("enter a noun","");
+var noun3 = prompt("enter a noun","");
+var noun4 = prompt("enter a noun","");
+var verb = prompt("enter a verb ending in -ing","");
+var verb2 = prompt("enter a past tense verb","");
+var verb3 = prompt("enter a past tense verb","");
+var verb4 = prompt("enter a verb","");
+var adjective = prompt("enter a adjective","");
+var adjective2 = prompt("enter a adjective","");
+
+document.getElementById("name").innerHTML= "One day I was "+verb+" and I "+verb2+" a big "+noun1+". "+"When I "+verb3+" the "+noun2+" over there was a "+adjective+", little "+noun3+". "+"I was like ''That log had a "+noun4+"''. "+"That is how you "+verb4+" a "+adjective2+" story."
+
